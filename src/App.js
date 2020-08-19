27e94e160f6610171b09e0c72226ee508c2821de
Michael <michaeljanschiumo@gmail.com>
2020-08-19 15:52:17 -0400

    added APP css from rftb

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Movie Maka</h1>
-      </header>
+      <h1>Hello World</h1>
     </div>
   );
 }
