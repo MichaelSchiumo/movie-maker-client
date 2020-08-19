afafbf6041bc13bafa7a341583cf584cb4ba5127
Michael <michaeljanschiumo@gmail.com>
2020-08-19 16:03:24 -0400

    converted app to functional cmpt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import './App.css'
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+const App = () => {
+  return <div>My App</div>;
+};
 
 export default App;
